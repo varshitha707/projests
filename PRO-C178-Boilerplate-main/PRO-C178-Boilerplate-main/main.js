3d6529a909cff08c49a2a3a908e0e23e7cd3b0b0
varshitha707 <124167125+varshitha707@users.noreply.github.com>
2023-10-25 18:29:27 +0530

    Add files via upload

---
+++ PRO-C178-Boilerplate-main/PRO-C178-Boilerplate-main/main.js
@@ -0,0 +1,22 @@
+let latitude = 22.7868, longitude = 88.364
+mapboxgl.accessToken='pk.eyJ1IjoiYXBvb3J2ZWxvdXMiLCJhIjoiY2ttZnlyMDgzMzlwNTJ4a240cmEzcG0xNyJ9.-nSyL0Gy2nifDibXJg4fTA'
+var map=new mapboxgl.Map({
+    container:'map',
+    style:'mapbox://styles/mapbox/streets-v11',
+    center:[longitude,latitude],
+    zoom:16
+})
+map.addControl(
+    new mapboxgl.GeolocateControl(
+        {
+            positionOptions:{enableHighAccuracy:true},
+            trackUserLocation:true
+        }
+    )
+)
+map.addControl(
+    new MapboxDirections({
+        accessToken:mapboxgl.accessToken
+    }),
+    'top-left'
+)
